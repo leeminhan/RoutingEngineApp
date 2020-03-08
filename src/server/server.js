03ff4c1bb70a3c68b7955ddd332270eca050e87e
leeminhan <minhan.lmh@gmail.com>
2020-03-08 19:13:19 +0800

    Setup of connection between Backend & RainbowNodeSDK done

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,50 @@
 const express = require('express');
-const rainbowSDK = require('rainbow-node-sdk')
+const RainbowSDK = require('rainbow-node-sdk')
 
 const app = express();
+
+// Define your configuration
+let options = {
+    rainbow: {
+        host: "sandbox"
+    },
+    credentials: {
+        login: "testacc1@gmail.com", // To replace by your developer credendials
+        password: "Sutd@1234" // To replace by your developer credentials
+    },
+    // Application identifier
+    application: {
+        appID: "be276f0060f911ea9a6dcf004cf8c14e",
+        appSecret: "BQZaEc577I0aTMo34piRvCo55caL1afKRuEtkeGNjqRmbwwRYVIWNCxqZ9GkTgD7"
+    },
+    // Logs options
+    logs: {
+        enableConsoleLogs: true,
+        enableFileLogs: false,
+        "color": true,
+        "level": 'debug',
+        "customLabel": "vincent01",
+        "system-dev": {
+            "internals": false,
+            "http": false,
+        }, 
+        file: {
+            path: "/var/tmp/rainbowsdk/",
+            customFileName: "R-SDK-Node-Sample2",
+            level: "debug",
+            zippedArchive : false/*,
+            maxSize : '10m',
+            maxFiles : 10 // */
+        }
+    },
+    // IM options
+    im: {
+        sendReadReceipt: true
+    }
+};
+
+const rainbow = new RainbowSDK(options);
+
 
 // Managing User & Guests
 
@@ -10,13 +53,12 @@ const app = express();
 // IM Service
 
 
-// Start the SDK
-rainbowSDK.start().then(() => {
+// 1. Start the SDK
+rainbow.start().then(() => {
     // Do something when the SDK is connected to Rainbow
     // ... 
+    console.log('Connected to Rainbow')
 });
-
-
 
 
 app.listen(8000, function() {
