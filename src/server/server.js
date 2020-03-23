804c9b3e954804deb8d9b1848f599e664b3ed829
taykiathong <49123732+taykiathong@users.noreply.github.com>
2020-03-23 20:07:45 +0800

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,29 @@
+require('dotenv').config()
+
 const express = require('express');
 const RainbowSDK = require('rainbow-node-sdk');
 const options = require('./options'); // Import Configurations
 
 const app = express();
+const mongoose = require('mongoose');
 
 const rainbow = new RainbowSDK(options);
 
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
 
-// Managing User & Guests
+app.listen(8000, function() {
+    console.log('App running on port 8000');
+});
+
+require('dotenv').config()
+
+
+
+
+
 
 // Create accounts on rbw CLI first 
 // Admins Service
