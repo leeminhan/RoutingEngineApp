4d833b08c50bd57904e642d6c5c8e90730c4c7bc
taykiathong <49123732+taykiathong@users.noreply.github.com>
2020-03-23 20:07:53 +0800

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+// front to back end
+// backend send to database
 
 
 module.exports =  router;
