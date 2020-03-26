8845a0957585a2c822f9d1e88346dd58226d935f
taykiathong <49123732+taykiathong@users.noreply.github.com>
2020-03-27 02:00:07 +0800

    Update user.test.js

--- src/server/user.test.js
+++ src/server/user.test.js
@@ -33,8 +33,51 @@ describe('Test insert function', () => {
   } 
   });
 
-  
+  it('search for doc in db', async () => {
+    const mockUser = {_id: '23121234', firstName: 'firstName', lastName: 'lastName', chatMode: "chat", top: "CPF Pay Out"};
+    const user = db1.collection("user");
 
+    try{
+      await user.insertOne(mockUser);
+      const resultUser = await db.search({id: "some_user_id"}, "user");
+      expect(resultUser).toEqual(mockUser);
+    } catch(err) {
+      console.log("User already exists!");
+    }
+  });
+
+  it('update doc in db', async () => {
+    try{
+      await db.update({firstName: "firstName"}, {lastName: "lastName"}, "user");
+      const user = db1.collection("user");
+      const resultUser = await user.findOne({id: "some_user_id"});
+      expect(resultUser.lastName).toBe("lastName");
+    } catch(err){
+      console.log("Error: " + err);
+    }
+  });
+
+  it('find docs matching query in db', async () => {
+    try{
+      const users = db.findAll({top: "CPF payout"}, "user");
+      const user = db1.collection("user");
+      const results = user.find({top: "CPF payout"}).toArray();
+      expect(results).toEqual(users);
+    } catch (err) {
+      console.log("Error: " + err);
+    }
+  });
+
+  it('delete doc in db', async () => {
+    try{
+      await db.delete({top: "CPF payout"}, "user");
+      const user = myDB.collection("user");
+      const resultUser = await user.findOne({top: "CPF payout"});
+      expect(resultUser).toBe(null);
+    } catch(err){
+      console.log("Error: " + err);
+    }
+  });
 });
 
 
