b92354ee744cbe0017dc88e181135074771c22b0
taykiathong <49123732+taykiathong@users.noreply.github.com>
2020-03-27 02:04:00 +0800

    Update on test cases

--- src/server/index.js
+++ src/server/index.js
@@ -42,36 +42,4 @@ router.post('/users', (req,res) => {
     }
 })
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'RoutingEngineDB'
-
- // Route to Create Guest Account --> Send back credentials to frontend
-
- MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
-    if(error){
-        return console.log("Unable to connect to database")
-    }
-    console.log("Connected correctly!")
-
-    const db = client.db(databaseName)
-    
-    db.collection('user').insertOne({
-        firstName:"firstName",
-        lastName: "lastName",
-        chatMode: "chat",
-        top: "CPF Pay Out/ Maybe problem 1"//top: type of problem
-    }).then((result) => {
-        console.log(result)
-    }).catch((error) => {
-        console.log(error)
-    })
-})
-
-// Route to upload new user information to Database
-// router.post('/upload', async(req,res) => {
-
-// })
-
-
-
-// module.exports =  router;
+module.exports =  router;
