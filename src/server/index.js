d73fea5659516799847e852f9b39b64c1672b1e9
taykiathong <49123732+taykiathong@users.noreply.github.com>
2020-04-08 02:49:37 +0800

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+// const app = express();
 const router = new express.Router();
 const axios = require('axios');
+
+
+const options = require('./options');
 const RainbowSDK = require('rainbow-node-sdk');
 
 const mongodb = require('mongodb')
@@ -9,17 +13,58 @@ const MongoClient = mongodb.MongoClient //gives us access to function necessary 
 const connectionURL = 'mongodb://localhost:27017'
 const databaseName = 'RoutingEngineDB'
 
-const queue = [] //Global Variable.
-const queue2 = [] //Global Variable.
-const queue3 = [] //Global Variable.
-const queue4 = [] //Global Variable.
+const rateLimit = require("express-rate-limit");
 
-// Route to Create Guest Account --> Send back credentials to frontend
+
+
+//limiter = require('express-limiter')(router, mongodb)
+// var mycollection = db.collection('user')
+// var limiter = require('express-limiter')(mycollection, 'mongodb')
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 1 // limit each IP to 1 requests per windowMs
+  });
+   
+  //  apply to all requests
+  //app.use(limiter);
+
+const rainbowSDK = new RainbowSDK(options);
+rainbowSDK.start();
+rainbowSDK.events.on('rainbow_onready', function() {
+    // do something when the SDK is connected to Rainbow
+    console.log('Successfully connected to rainbow NodeSDK')
+});
+
+// //setting of limiter
+// limiter({
+//     method: 'post',
+//     lookup: ['connection.remoteAddress'],
+//     // 1 requests per min
+//     total: 1,
+//     expire: 60000
+//   })
+
+
+/* Admin Service - Create Guest Account For User*/
+router.post("/", limiter, (req,res) => {
+    try{
+        rainbowSDK.admin.createAnonymousGuestUser().then((loginCredentials) => {
+            console.log("Guest User Account successfully created")
+            console.log(loginCredentials)
+            res.status(200).send(loginCredentials)
+        }).catch((error)=>{
+            console.log(`Failed to Create Guest Account; Error: ${error}`)
+        })
+
+    }catch(error){
+        res.status(404).send(error)
+        console.log("Creating of Guess Account Failed")
+    }
+})
 
 // Route to upload new user information to Database
-
-//---------------------------------  HTTP REQUEST '/users' Handler START ------------------------------------------------------------------
-router.post('/users', (req,res) => {
+router.post('/users', limiter, (req,res) => {
     const info = req.body
     console.log(info)
 
@@ -58,11 +103,11 @@ router.post('/users', (req,res) => {
 //---------------------------------  HTTP REQUEST Handler END ------------------------------------------------------------------
 
 //---------------------------------  HTTP REQUEST '/connect' Handler START -----------------------------------------------------------------
-router.post('/connect', (req,res) => {
-    const info = req.body
-    console.log(info)
-    queue.push(info.firstname)
-    console.log(queue)
+// router.post('/connect', (req,res) => {
+//     const info = req.body
+//     console.log(info)
+//     queue.push(info.firstname)
+//     console.log(queue)
 
     /*
         1) First check agent is available
@@ -73,7 +118,6 @@ router.post('/connect', (req,res) => {
         3) If available
         4) 
     */
-})
+// })
 
-//---------------------------------  HTTP REQUEST '/connect' Handler END ----------------------------------------------------
-module.exports =  router;
+module.exports = router;
