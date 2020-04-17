a7f72d3897c12cff6b486c1253f92eca76945bee
leeminhan <minhan.lmh@gmail.com>
2020-04-17 23:56:41 +0800

    update to db.js - insertHandler, findHandler and /agent route

--- src/server/db.js
+++ src/server/db.js
@@ -7,18 +7,48 @@ const databaseName = 'RoutingEngineDB'
 const client = new MongoClient(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true});
 
 const insertHandler = async(data, collectionName) => {
-
+    // client.connect() should one called once -- IMPORTANT
     await client.connect().then(async() => {
         console.log("insertHandler: Connected to Database")
-        console.log("Collection Name:", collectionName)
+        // console.log("Collection Name:", collectionName)
         const collection = client.db(databaseName).collection(collectionName)
         await collection.insertOne(data)
-        console.log("Successfully inserted to database")
+        console.log("insertHandler --- end \n")
     }).catch(error => {
         console.log("Failed to insert:", error)
     })
 }
 
+const findHandler = async(query, collectionName) => {
+
+    try{
+        console.log("findHandler: Connected to Database")
+        // console.log("Collection Name:", collectionName)
+        const collection = client.db(databaseName).collection(collectionName)
+        const result = await collection.findOne(query)
+        console.log("findHandler --- end \n")
+        return result
+
+    }catch(error){
+        console.log("Failed to insert:", error)
+    }
+}
+
+const updateHandler = async(data, collectionName) => {
+
+    await client.connect().then(async() => {
+        console.log("updateHandler: Connected to Database")
+        // console.log("Collection Name:", collectionName)
+        const collection = client.db(databaseName).collection(collectionName)
+        await collection.updateOne(data)
+        ////missing code here
+        console.log("Successfully udpated to database:", data)
+    }).catch(error => {
+        console.log("Failed to insert:", error)
+    })
+}
 module.exports = {
-    insertHandler: insertHandler
+    insertHandler: insertHandler,
+    updateHandler: updateHandler,
+    findHandler: findHandler
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = new express.Router();
-const axios = require('axios');
+const db = require('./db')
 
 const options = require('./options');
 const RainbowSDK = require('rainbow-node-sdk');
@@ -45,7 +45,7 @@ router.post("/", (req,res) => {
     try{
         rainbowSDK.admin.createAnonymousGuestUser().then((loginCredentials) => {
             console.log("Guest User Account successfully created")
-            console.log("Login Credentials:\n" , loginCredentials)
+            // console.log("Login Credentials:\n" , loginCredentials)
             res.status(200).send(loginCredentials)
         }).catch((error)=>{
             console.log(`Failed to Create Guest Account; Error: ${error}`)
@@ -58,95 +58,102 @@ router.post("/", (req,res) => {
 })
 
 // Route to upload new user information to Database
-router.post('/users',(req,res) => {
+router.post('/users',async(req,res) => {
     const userInfo = req.body
-    console.log(userInfo)
     try{
-        MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
-            if(error){
-                return console.log("Unable to connect to database")
-            }
-            console.log("Connected to Database successfully!")
-        
-            const db = client.db(databaseName)
-            
-            db.collection('user').insertOne({
-                userInfo
-            }).then((result) => {
-                res.status(200).send(userInfo)
-            }).catch((error) => {
-                console.log(error)
-            })
-        })
+        await db.insertHandler(userInfo, "user")
+        console.log("Successfully inserted to database:", userInfo)
+        // console.log("Uploaded to userInfo to DB:", userInfo)
+        res.status(200).send()
     }catch(error){
         res.status(500).send()
     }
 })
 
-router.post('/agents', (req,res) => {
+router.post('/agents', async(req,res) => {
     const userInfo = req.body
-
-    // 1. Check Agent's TOP and Availabiliity 
+    const query = {
+        top: userInfo.top
+    }
+    console.log("userInfo:", query)
+    
     try{
-        MongoClient.connect(connectionURL, {useNewUrlParser: true}, async(error, client) => {
-            if(error){
-                return console.log("Unable to connect to database")
-            }
-            console.log("Agent Route: Connected to Agent's Database successfully!")
-            const db = client.db(databaseName)
+        const agent = await db.findHandler(query, 'agent')
+        console.log("Agent Route: Successfully queried database. Result:\n", agent)
+        console.log("agentID:", agent.agentId)
+        
+        const agentAvailability = await checkAgentStatusHandler(agent.agentId)
+        console.log("agentAvailability", agentAvailability)
+        
+        if(agentAvailability == 'online'){
+            // Update agent availbility/status to DB?
+            console.log(`Agent ${agent.agentId} is available. Connecting with user now!`)
+            res.status(200).send({agentId: agent.agentId, presence: agentAvailability})
+        }
+        res.status(200).send()
+        // if(agent.top == 'busy'){
 
-            // 1. Look for agent with matching TOP
-            // found: -> then block: check agent availbility 
-            // -> if yes, return agentId to frontend as input for searchByIdHandler to get agentObject to openConvo
-            // 
-            // not found: -> catch error block
+        // }
 
-            
-            db.collection('agent').findOne({ //note to self: .find no longer works; documentation not updated
-                top: userInfo.top
-            }).then(async(agentDb) => {
-                console.log("Found Agent with matching TOP! Checking availability -----")
-                console.log("AgentDB result:", agentDb)
-                // Check agent availability
+        // MongoClient.connect(connectionURL, {useNewUrlParser: true}, async(error, client) => {
+        //     if(error){
+        //         return console.log("Unable to connect to database")
+        //     }
+        //     console.log("Agent Route: Connected to Agent's Database successfully!")
+        //     const db = client.db(databaseName)
 
-                const agentId = agentDb.agentId
-                // console.log("AgentId:", agentId) 
-                // const temp1 = "5e5fdf3bd8084c29e64eb20a"
-                // const temp = "5e84513235c8367f99b94cee"
-                const availability = await checkAgentStatusHandler(agentId)
-                console.log("Agent's Availability:", availability)
+        //     // 1. Look for agent with matching TOP
+        //     // found: -> then block: check agent availbility 
+        //     // -> if yes, return agentId to frontend as input for searchByIdHandler to get agentObject to openConvo
+        //     // 
+        //     // not found: -> catch error block
 
-                if(availability == "online"){
-                    // Match agent to user -> Update agent to busy
-                    db.collection('agent').updateOne({
-                        agentId: agentId
-                    },{
-                        $set:{
-                            availability: "busy" 
-                        }
-                    }).then((result) => {
-                        console.log("Agent Availability Updated:", result)
-                    }).catch(error => {
-                        console.log("Failed to update agent")
-                        console.log(error)
-                    })
-                    res.status(200).send(availability)
-                }
+        //     db.collection('agent').findOne({ //note to self: .find no longer works; documentation not updated
+        //         top: userInfo.top
+        //     }).then(async(agentDb) => {
+        //         console.log("Found Agent with matching TOP! Checking availability -----")
+        //         console.log("AgentDB result:", agentDb)
+        //         // Check agent availability
+
+        //         const agentId = agentDb.agentId
+        //         // console.log("AgentId:", agentId) 
+        //         // const temp1 = "5e5fdf3bd8084c29e64eb20a"
+        //         // const temp = "5e84513235c8367f99b94cee"
                 
-                // res.status(200).send(availability)
+        //         const availability = await checkAgentStatusHandler(agentId)
+        //         console.log("Agent's Availability:", availability)
+            
+        //         if(availability == "online"){
+        //             // Match agent to user -> Update agent to busy
+        //             db.collection('agent').updateOne({
+        //                 agentId: agentId
+        //             },{
+        //                 $set:{
+        //                     availability: "busy" 
+        //                 }
+        //             }).then((result) => {
+        //                 console.log("Agent Availability Updated:", result)
+        //             }).catch(error => {
+        //                 console.log("Failed to update agent")
+        //                 console.log(error)
+        //             })
+        //             res.status(200).send(availability)
+        //         }
+                
+        //         // res.status(200).send(availability)
 
-            }).catch((error) => {
-                console.log("No Agent with matching TOP found!")
-                console.log(error)
-            })    
-        })
+        //     }).catch((error) => {
+        //         console.log("No Agent with matching TOP found!")
+        //         console.log(error)
+        //     })    
+        // })
+
+
+
     }catch(error){
+        console.log("\n/agents route: Failed to Connect to MongoDB")
         res.status(500).send()
     }
-
-    // if yes
-
-    // else put into the queue
 })
 
 module.exports =  router;
