9b286c798f78d74eba5ce03871b42850d442e642
leeminhan <minhan.lmh@gmail.com>
2020-04-19 15:41:55 +0800

    Added and fixed findConditionHandler() & deleteHandler

--- src/server/db.js
+++ src/server/db.js
@@ -34,14 +34,57 @@ const findHandler = async(query, collectionName) => {
     }
 }
 
-const updateHandler = async(data, collectionName) => {
+// For User with specific TOP and shortest timestamp
+const findConditionHandler = async(query, collectionName) => {
+
+    try{
+        console.log("findHandler: Connected to Database")
+        // console.log("Collection Name:", collectionName)
+        const collection = client.db(databaseName).collection(collectionName)
+        const result = await collection.find(query).next() //next() will go to the first entry
+        console.log("findHandler --- end \n")
+        console.log("result:",result)
+        console.log(typeof(result))
+        return result
+
+    }catch(error){
+        console.log("Failed to insert:", error)
+    }
+}
+
+const deleteHandler = async(query, collectionName) => {
+    try{
+        console.log("deleteHandler: Connected to Database")
+        const collection = client.db(databaseName).collection(collectionName)
+        console.log("deleteHandler --- end \n")
+        await collection.deleteOne(query) //note there is no await as we need this object returned to perform other filters
+        
+    }catch(error){
+        console.log("Failed to delete:", error)
+    }
+}
+
+const updateHandler = async(data, update, collectionName) => {
 
     await client.connect().then(async() => {
         console.log("updateHandler: Connected to Database")
         // console.log("Collection Name:", collectionName)
         const collection = client.db(databaseName).collection(collectionName)
-        await collection.updateOne(data)
-        ////missing code here
+        await collection.updateOne(data, update)
+
+        // db.collection('agent').updateOne({
+        //     agentId: agentId
+        // },{
+        //     $set:{
+        //         availability: "busy" 
+        //     }
+        // }).then((result) => {
+        //     console.log("Agent Availability Updated:", result)
+        // }).catch(error => {
+        //     console.log("Failed to update agent")
+        //     console.log(error)
+        // })
+
         console.log("Successfully udpated to database:", data)
     }).catch(error => {
         console.log("Failed to insert:", error)
@@ -50,5 +93,7 @@ const updateHandler = async(data, collectionName) => {
 module.exports = {
     insertHandler: insertHandler,
     updateHandler: updateHandler,
-    findHandler: findHandler
+    findHandler: findHandler,
+    deleteHandler: deleteHandler,
+    findConditionHandler: findConditionHandler
 }
