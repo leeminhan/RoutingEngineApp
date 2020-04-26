330605c11f0b2a7ae4114e7e41e17cdbadd1db9d
leeminhan <minhan.lmh@gmail.com>
2020-04-26 18:46:37 +0800

    Update to repo files

--- src/containers/Config.js
+++
@@ -1,6 +0,0 @@
-let config = { //rbw applications
-    applicationID: "be276f0060f911ea9a6dcf004cf8c14e",
-    applicationSecret: "BQZaEc577I0aTMo34piRvCo55caL1afKRuEtkeGNjqRmbwwRYVIWNCxqZ9GkTgD7"
-}
-
-module.exports = config
